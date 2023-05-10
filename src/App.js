4a6f479ff7780abfef9b8f0814b8fbde868b5928
johnr21 <johnrabelo.contact@gmail.com>
2023-05-10 20:19:23 -0300

    xhrAdded

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
+
+  function TOff (){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://192.168.0.51/gpio/1');
+    xhr.send();
+  }
+
+
+  function TOn (){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://192.168.0.51/gpio/0');
+    xhr.send();
+  }
+
   return (
     <div>
-      My most recent project. Updates coming soon!
+      <button onClick={TOn}>Turn On</button>
+      <button onClick={TOff}>Turn Off</button>
     </div>
   );
 }
