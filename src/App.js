7fd6d33204431607c90ea516de7551ec9ebc1967
johnr21 <johnrabelo.contact@gmail.com>
2023-05-12 16:21:02 -0300

    addtagbutton

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ function App() {
     <div>
       <button onClick={TOn}>Turn On</button>
       <button onClick={TOff}>Turn Off</button>
-      <p>New features coming soon</p>
+      <p>New features coming up soon</p>
+      <button>Add a tag</button>
     </div>
   );
 }
